94e9d955bd178b0a35e20dedd613bcd6924c83ee
kr37 <kelsangrinzin@gmail.com>
2015-12-08 00:25:29 -0700

    onAfterInstall initial success
    Really need to clean it up though

--- calendar37/Calendar37Plugin.php
+++ calendar37/Calendar37Plugin.php
@@ -76,12 +76,24 @@ class Calendar37Plugin extends BasePlugin
 	}
 
 	public function onAfterInstall() {
-		savePluginSettings($this, $this->getDefaultSettings());
+		$settings['status'] = 'a';
+		$settings['showTails'] = 'b';
+		$settings['dateformat'] = 'c';
+		$settings['dateformat1st'] = 'd';
+		$settings['occurrenceFormat'] = 'e';
+		$settings['availableTimes'] = 'f';
+		$settings['mySetting'] = 'g';
+		$settings['categoryFieldHandle'] = 'h';
+		$settings['entryCalendarTextFieldHandle'] = 'i';
+		$settings['cssFieldHandle'] = 'j';
+		$settings['startDateFieldHandle'] = 'k';
+		$success = craft()->plugins->savePluginSettings($this, $settings);
+		if (!$success) die ("Failed to set initial settings. (But the plugin should be installed now)" );
 	}
 
 	public function getDefaultSettings() {
 		return array (
-			'cpCssFile' => '',
+			'cpCssFile' => 'x',
 			'occurrenceFormat' => '%s %s',
 			'availableTimes' => '<option value="">Choose the time...</option><option value="-1">All day/no time - top spot</option><option value="-2">All day/no time - 2nd spot</option><option value="-3">All day/no time - 3rd spot</option><option value="25">All day/no time - 3rd from bottom</option><option value="26">All day/no time - 2nd from bottom</option><option value="27">All day/no time - bottom spot</option><option value="06:30:00">6:30 am</option><option value="07:00:00">7:00 am</option><option value="07:30:00">7:30 am</option><option value="08:00:00">8:00 am</option><option value="08:30:00">8:30 am</option><option value="09:00:00">9:00 am</option><option value="09:30:00">9:30 am</option><option value="10:00:00">10:00 am</option><option value="10:30:00">10:30 am</option><option value="11:00:00">11:00 am</option><option value="11:30:00">11:30 am</option><option value="12:00:00">12:00 pm</option><option value="12:30:00">12:30 pm</option><option value="13:00:00">1:00 pm</option><option value="13:30:00">1:30 pm</option><option value="14:00:00">2:00 pm</option><option value="14:30:00">2:30 pm</option><option value="15:00:00">3:00 pm</option><option value="15:30:00">3:30 pm</option><option value="16:00:00">4:00 pm</option><option value="16:30:00">4:30 pm</option><option value="17:00:00">5:00 pm</option><option value="17:30:00">5:30 pm</option><option value="18:00:00">6:00 pm</option><option value="18:30:00">6:30 pm</option><option value="19:00:00">7:00 pm</option><option value="19:30:00">7:30 pm</option><option value="20:00:00">8:00 pm</option><option value="20:30:00">8:30 pm</option><option value="21:00:00">9:00 pm</option><option value="21:30:00">9:30 pm</option><option value="22:00:00">10:00 pm</option><option value="22:30:00">10:30 pm</option><option value="23:00:00">11:00 pm</option><option value="23:30:00">11:30 pm</option><option value="00:00:00">12:00 am</option><option value="00:30:00">12:30 am</option><option value="01:00:00">1:00 am</option><option value="01:30:00">1:30 am</option><option value="02:00:00">2:00 am</option><option value="02:30:00">2:30 am</option><option value="03:00:00">3:00 am</option><option value="03:30:00">3:30 am</option><option value="04:00:00">4:00 am</option><option value="04:30:00">4:30 am</option><option value="05:00:00">5:00 am</option><option value="05:30:00">5:30 am</option><option value="06:00:00">6:00 am</option>',
 			'status' => 'live pending expired',
@@ -97,12 +109,12 @@ class Calendar37Plugin extends BasePlugin
 			'dateformat1st' => 'D, F j',
 			'timezone' => '',
 
-			'categoryFieldHandle' => '',
-			'entryCalendarTextFieldHandle' => '',
-			'imageFieldHandle' => '',
-			'cssFieldHandle' => '',
+			'categoryFieldHandle' => 'x',
+			'entryCalendarTextFieldHandle' => 'x',
+			'imageFieldHandle' => 'x',
+			'cssFieldHandle' => 'x',
 			'startDateFieldHandle' => 'posted',
-			'urlFieldHandle' => ''
+			'urlFieldHandle' => 'x'
 		);
 	}
 
